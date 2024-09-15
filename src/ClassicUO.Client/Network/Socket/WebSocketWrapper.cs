285e9e10b4a8848e069b5e9f0e5b2c39ac8692af
Stefano Merotta <manelli80@hotmail.com>
2024-09-15 17:25:05 +0200

    Tiny refactor of WebSocketWrapper

--- src/ClassicUO.Client/Network/Socket/WebSocketWrapper.cs
+++ src/ClassicUO.Client/Network/Socket/WebSocketWrapper.cs
@@ -1,3 +1,4 @@
+using ClassicUO.Utility.Logging;
 using System;
 using System.Net;
 using System.Net.Http;
@@ -5,9 +6,8 @@ using System.Net.Sockets;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
-using ClassicUO.Utility.Logging;
-using TcpSocket = System.Net.Sockets.Socket;
 using static System.Buffers.ArrayPool<byte>;
+using TcpSocket = System.Net.Sockets.Socket;
 
 namespace ClassicUO.Network.Socket;
 
@@ -15,25 +15,25 @@ namespace ClassicUO.Network.Socket;
 /// Handles websocket connections to shards that support it. `ws(s)://[hostname]` as the ip in settings.json.
 /// For testing see `tools/ws/README.md` 
 /// </summary>
-sealed class WebSocketWrapper : SocketWrapper
+internal sealed class WebSocketWrapper : SocketWrapper
 {
     private const int MAX_RECEIVE_BUFFER_SIZE = 1024 * 1024; // 1MB
-    private const int WS_KEEP_ALIVE_INTERVAL = 5;            // seconds
+    private const int WS_KEEP_ALIVE_INTERVAL = 15;           // seconds
 
     private ClientWebSocket _webSocket;
     private TcpSocket _rawSocket;
+    private CancellationTokenSource _tokenSource;
+    private CircularBuffer _receiveStream;
+    private Task _webSocketClientTask;
 
     public override bool IsConnected => _webSocket?.State is WebSocketState.Connecting or WebSocketState.Open;
     public override EndPoint LocalEndPoint => _rawSocket?.LocalEndPoint;
     public bool IsCanceled => _tokenSource.IsCancellationRequested;
 
-    private CancellationTokenSource _tokenSource = new();
-    private CircularBuffer _receiveStream;
-
-    public override void Connect(Uri uri) => ConnectAsync(uri, _tokenSource).Wait();
-
-    public override void Send(byte[] buffer, int offset, int count) =>
-        _webSocket.SendAsync(buffer.AsMemory().Slice(offset, count), WebSocketMessageType.Binary, true, _tokenSource.Token);
+    public override void Send(byte[] buffer, int offset, int count)
+    {
+        _webSocket.SendAsync(new ArraySegment<byte>(buffer, offset, count), WebSocketMessageType.Binary, true, _tokenSource.Token).Wait();
+    }
 
     public override int Read(byte[] buffer)
     {
@@ -43,32 +43,32 @@ sealed class WebSocketWrapper : SocketWrapper
         }
     }
 
-    public async Task ConnectAsync(Uri uri, CancellationTokenSource tokenSource = null)
+    public override void Connect(Uri uri)
     {
         if (IsConnected)
             return;
 
-        _tokenSource = tokenSource ?? new CancellationTokenSource();
         _receiveStream = new CircularBuffer();
+        _tokenSource = new();
 
         try
         {
-            await ConnectWebSocketAsyncCore(uri);
+            ConnectWebSocketCore(uri, _tokenSource.Token).Wait();
 
             if (IsConnected)
                 InvokeOnConnected();
             else
                 InvokeOnError(SocketError.NotConnected);
         }
-        catch (WebSocketException ex)
+        catch (AggregateException aex) when (aex.InnerException is WebSocketException wse)
         {
-            SocketError error = ex.InnerException?.InnerException switch
+            SocketError error = wse.InnerException?.InnerException switch
             {
                 SocketException socketException => socketException.SocketErrorCode,
                 _ => SocketError.SocketError
             };
 
-            Log.Error($"Error {ex.GetType().Name} {error} while connecting to {uri} {ex}");
+            Log.Error($"Error {wse.GetType().Name} {error} while connecting to {uri} {wse}");
             InvokeOnError(error);
         }
         catch (Exception ex)
@@ -79,26 +79,26 @@ sealed class WebSocketWrapper : SocketWrapper
     }
 
 
-    private async Task ConnectWebSocketAsyncCore(Uri uri)
+    private async Task ConnectWebSocketCore(Uri uri, CancellationToken token)
     {
         // Take control of creating the raw socket, turn off Nagle, also lets us peek at `Available` bytes.
         _rawSocket = new TcpSocket(SocketType.Stream, ProtocolType.Tcp)
         {
-            NoDelay = true
+            NoDelay = true,
         };
 
         _webSocket = new ClientWebSocket();
         _webSocket.Options.KeepAliveInterval = TimeSpan.FromSeconds(WS_KEEP_ALIVE_INTERVAL); // ping/pong
 
-        using var httpClient = new HttpClient
+        var httpClient = new HttpClient
         (
-            new SocketsHttpHandler
+            new SocketsHttpHandler()
             {
                 ConnectCallback = async (context, token) =>
                 {
                     try
                     {
-                        await _rawSocket.ConnectAsync(context.DnsEndPoint, token);
+                        await _rawSocket.ConnectAsync(context.DnsEndPoint);
 
                         return new NetworkStream(_rawSocket, ownsSocket: true);
                     }
@@ -115,28 +115,27 @@ sealed class WebSocketWrapper : SocketWrapper
             }
         );
 
-
-        await _webSocket.ConnectAsync(uri, httpClient, _tokenSource.Token);
-
+        await _webSocket.ConnectAsync(uri, httpClient, token);
         Log.Trace($"Connected WebSocket: {uri}");
 
-        // Kicks off the async receiving loop 
-        StartReceiveAsync().ConfigureAwait(false);
+        _webSocketClientTask = StartReceiveAsync(token);
     }
 
-    private async Task StartReceiveAsync()
+    private async Task StartReceiveAsync(CancellationToken token)
     {
+        var socket = _webSocket;
+        var rawSocket = _rawSocket;
         var buffer = Shared.Rent(4096);
-        var memory = buffer.AsMemory();
         var position = 0;
+        var lastState = WebSocketState.Open;
 
         try
         {
-            while (IsConnected)
+            while (!token.IsCancellationRequested && socket.State is WebSocketState.Open)
             {
-                GrowReceiveBufferIfNeeded(ref buffer, ref memory);
+                GrowReceiveBufferIfNeeded(ref buffer, position, position + rawSocket.Available);
 
-                var receiveResult = await _webSocket.ReceiveAsync(memory.Slice(position), _tokenSource.Token);
+                var receiveResult = await socket.ReceiveAsync(new ArraySegment<byte>(buffer, position, buffer.Length - position), token);
 
                 // Ignoring message types:
                 // 1. WebSocketMessageType.Text: shouldn't be sent by the server, though might be useful for multiplexing commands
@@ -154,52 +153,69 @@ sealed class WebSocketWrapper : SocketWrapper
 
                 position = 0;
             }
+
+            lastState = socket.State;
         }
         catch (OperationCanceledException)
         {
-            Log.Trace("WebSocket OperationCanceledException on websocket " + (IsCanceled ? "(was requested)" : "(remote cancelled)"));
+            Log.Trace("WebSocket OperationCanceledException on websocket " + (token.IsCancellationRequested ? "(was requested)" : "(remote cancelled)"));
         }
         catch (Exception e)
         {
             Log.Trace($"WebSocket error in StartReceiveAsync {e}");
+            socket.Abort();
+
             InvokeOnError(SocketError.SocketError);
         }
         finally
         {
             Shared.Return(buffer);
+            socket.Dispose();
         }
 
-        if (!IsCanceled)
-            InvokeOnError(SocketError.ConnectionReset);
+        if (!token.IsCancellationRequested)
+        {
+            if (lastState == WebSocketState.CloseReceived)
+                InvokeOnDisconnected();
+            else
+                InvokeOnError(SocketError.ConnectionReset);
+        }
     }
 
     // This is probably unnecessary, but WebSocket frames can be up to 2^63 bytes so we put some cap on it, yet to see packets larger than 4KB come through.
     // We peek the raw tcp socket available bytes, grow if the frame is bigger, we're naively assuming no compression.
-    private void GrowReceiveBufferIfNeeded(ref byte[] buffer, ref Memory<byte> memory)
+    private void GrowReceiveBufferIfNeeded(ref byte[] buffer, int oldLength, int requiredLength)
     {
-        if (_rawSocket.Available <= buffer.Length)
+        if (requiredLength <= buffer.Length)
             return;
 
-        if (_rawSocket.Available > MAX_RECEIVE_BUFFER_SIZE)
+        if (requiredLength > MAX_RECEIVE_BUFFER_SIZE)
             throw new SocketException((int)SocketError.MessageSize, $"WebSocket message frame too large: {_rawSocket.Available} > {MAX_RECEIVE_BUFFER_SIZE}");
 
         Log.Trace($"WebSocket growing receive buffer {buffer.Length} bytes to {_rawSocket.Available} bytes");
 
-        Shared.Return(buffer);
-        buffer = Shared.Rent(_rawSocket.Available);
-        memory = buffer.AsMemory();
+        var old = buffer;
+
+        buffer = Shared.Rent(requiredLength);
+        Array.Copy(old, 0, buffer, 0, oldLength);
+
+        Shared.Return(old);
     }
 
     public override void Disconnect()
     {
-        if (!IsConnected)
-            return;
+        _tokenSource.Cancel();
 
-        _tokenSource?.Cancel();
-        _webSocket?.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnect", _tokenSource?.Token ?? default);
+        if (_webSocketClientTask != null)
+        {
+            _webSocketClientTask.Wait();
+            _webSocketClientTask = null;
+        }
     }
 
     public override void Dispose()
     {
+        if (_webSocketClientTask != null)
+            Disconnect();
     }
 }
