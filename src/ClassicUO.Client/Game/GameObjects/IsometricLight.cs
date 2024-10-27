5708cae48e1b4b07017edca5905316bd6c5f3dae
Stefano Merotta <97297186+stefanomerotta@users.noreply.github.com>
2024-10-27 14:44:36 +0100

    Some code cleanup Added license to editorconfig

--- src/ClassicUO.Client/Game/GameObjects/IsometricLight.cs
+++ src/ClassicUO.Client/Game/GameObjects/IsometricLight.cs
@@ -30,76 +30,31 @@
 
 #endregion
 
-using Microsoft.Xna.Framework;
+namespace ClassicUO.Game.GameObjects;
 
-namespace ClassicUO.Game.GameObjects
+#nullable enable
+
+internal sealed class IsometricLight
 {
-    internal sealed class IsometricLight
+    public int Overall { get; set { field = value; Recalculate(); } }
+    public int Personal { get; set { field = value; Recalculate(); } }
+    public int RealOverall { get; set { field = value; Recalculate(); } }
+    public int RealPersonal { get; set { field = value; Recalculate(); } }
+    public float IsometricLevel { get; private set; }
+
+    public IsometricLight(int allLevels = 9)
     {
-        private float _height = -0.75f;
-        private int _overall = 9, _realOveall = 9;
-        private int _personal = 9, _realPersonal = 9;
+        Overall = allLevels;
+        Personal = allLevels;
+        RealOverall = allLevels;
+        RealPersonal = allLevels;
+    }
 
-        public int Personal
-        {
-            get => _personal;
-            set
-            {
-                _personal = value;
-                Recalculate();
-            }
-        }
+    private void Recalculate()
+    {
+        int reverted = 32 - Overall; //if overall is 0, we have MAXIMUM light, if 30, we have the MINIMUM light, so 30 is the max, but we must have some remainder for visibility
 
-        public int Overall
-        {
-            get => _overall;
-            set
-            {
-                _overall = value;
-                Recalculate();
-            }
-        }
-
-        public float Height
-        {
-            get => _height;
-            set
-            {
-                _height = value;
-                Recalculate();
-            }
-        }
-
-        public int RealPersonal
-        {
-            get => _realPersonal;
-            set
-            {
-                _realPersonal = value;
-                Recalculate();
-            }
-        }
-
-        public int RealOverall
-        {
-            get => _realOveall;
-            set
-            {
-                _realOveall = value;
-                Recalculate();
-            }
-        }
-
-        public float IsometricLevel { get; private set; }
-
-        public Vector3 IsometricDirection { get; } = new Vector3(-1.0f, -1.0f, .5f);
-
-        private void Recalculate()
-        {
-            int reverted = 32 - Overall; //if overall is 0, we have MAXIMUM light, if 30, we have the MINIMUM light, so 30 is the max, but we must have some remainder for visibility
-
-            float current = Personal > reverted ? Personal : reverted;
-            IsometricLevel = current * 0.03125f;
-        }
+        float current = Personal > reverted ? Personal : reverted;
+        IsometricLevel = current * 0.03125f;
     }
 }
--- src/ClassicUO.Client/Game/LinkedObject.cs
+++ src/ClassicUO.Client/Game/LinkedObject.cs
@@ -33,285 +33,247 @@
 using System;
 using System.Diagnostics;
 
-namespace ClassicUO.Game
+namespace ClassicUO.Game;
+
+#nullable enable
+
+internal abstract class LinkedObject
 {
-    internal abstract class LinkedObject
+    public LinkedObject? Previous;
+    public LinkedObject? Next;
+    public LinkedObject? Items;
+
+    public bool IsEmpty => Items is null;
+
+    public void PushToBack(LinkedObject item)
     {
-        public bool IsEmpty => Items == null;
-        public LinkedObject Previous, Next, Items;
+        if (item is null)
+            return;
 
-        //~LinkedObject()
-        //{
-        //    Clear();
+        Remove(item);
 
-        //    LinkedObject item = Next;
-
-        //    while (item != null && item != this)
-        //    {
-        //        LinkedObject next = item.Next;
-        //        item.Next = null;
-        //        item = next;
-        //    }
-        //}
-
-        public void PushToBack(LinkedObject item)
+        if (Items is null)
         {
-            if (item == null)
-            {
-                return;
-            }
-
-            Remove(item);
-
-            if (Items == null)
-            {
-                Items = item;
-            }
-            else
-            {
-                LinkedObject last = GetLast();
-                last.Next = item;
-
-                Debug.Assert(item.Next == null, "[Append to last-next] item must be unlinked before.");
-                item.Next = null;
-                item.Previous = last;
-            }
+            Items = item;
+            return;
         }
 
-        public void Remove(LinkedObject item)
-        {
-            if (item == null)
-            {
-                return;
-            }
+        LinkedObject? last = GetLast();
+        last.Next = item;
 
-            Unlink(item);
-            item.Next = null;
+        Debug.Assert(item.Next is null, "[Append to last-next] item must be unlinked before.");
+
+        item.Next = null;
+        item.Previous = last;
+    }
+
+    public void Remove(LinkedObject item)
+    {
+        if (item is null)
+            return;
+
+        Unlink(item);
+        item.Next = null;
+        item.Previous = null;
+    }
+
+    public void Unlink(LinkedObject item)
+    {
+        if (item is null)
+            return;
+
+        if (item == Items)
+        {
+            Items = Items.Next;
+
+            if (Items is not null)
+                Items.Previous = null;
+        }
+        else
+        {
+            if (item.Previous is not null)
+                item.Previous.Next = item.Next;
+
+            if (item.Next is not null)
+                item.Next.Previous = item.Previous;
+        }
+    }
+
+    public void Insert(LinkedObject first, LinkedObject item)
+    {
+        if (first is null)
+        {
+            item.Next = Items;
             item.Previous = null;
+
+            if (Items is not null)
+                Items.Previous = item;
+
+            Items = item;
+        }
+        else
+        {
+            LinkedObject? next = first.Next;
+            item.Next = next;
+            item.Previous = first;
+            first.Next = item;
+
+            if (next is not null)
+                next.Previous = item;
+        }
+    }
+
+    public void MoveToFront(LinkedObject item)
+    {
+        if (item is not null && item != Items)
+        {
+            Unlink(item);
+
+            if (Items is not null)
+                Items.Previous = item;
+
+            item.Next = Items;
+            item.Previous = null;
+            Items = item;
+        }
+    }
+
+    public void MoveToBack(LinkedObject item)
+    {
+        if (item is null)
+            return;
+
+        Unlink(item);
+        LinkedObject? last = GetLast();
+
+        if (last is null)
+            Items = item;
+        else
+            last.Next = item;
+
+        item.Previous = last;
+        item.Next = null;
+    }
+
+    public LinkedObject? GetLast()
+    {
+        LinkedObject? last = Items;
+
+        while (last is not null && last.Next is not null)
+        {
+            last = last.Next;
         }
 
-        public void Unlink(LinkedObject item)
+        return last;
+    }
+
+    public void Clear()
+    {
+        if (Items is null)
+            return;
+
+        LinkedObject? item = Items;
+        Items = null;
+
+        while (item is not null)
         {
-            if (item == null)
-            {
-                return;
-            }
-
-            if (item == Items)
-            {
-                Items = Items.Next;
-
-                if (Items != null)
-                {
-                    Items.Previous = null;
-                }
-            }
-            else
-            {
-                if (item.Previous != null)
-                {
-                    item.Previous.Next = item.Next;
-                }
-
-                if (item.Next != null)
-                {
-                    item.Next.Previous = item.Previous;
-                }
-            }
+            LinkedObject? next = item.Next;
+            item.Next = null;
+            item = next;
         }
+    }
 
-        public void Insert(LinkedObject first, LinkedObject item)
+    /// <summary>
+    ///     Sort the contents of this LinkedObject using merge sort.
+    ///     Adapted from Simon Tatham's C implementation: https://www.chiark.greenend.org.uk/~sgtatham/algorithms/listsort.html
+    /// </summary>
+    /// <typeparam name="T">Type of the objects being compared.</typeparam>
+    /// <param name="comparison">Comparison function to use when sorting.</param>
+    public LinkedObject? SortContents<T>(Comparison<T> comparison) where T : LinkedObject
+    {
+        if (Items is null)
+            return null;
+
+        int unitsize = 1; //size of the components we are merging; 1 for first iteration, multiplied by 2 after each iteration
+
+        T? p = null;
+        T? q;
+        T? e;
+        T? head = (T)Items;
+        T? tail;
+
+        while (true)
         {
-            if (first == null)
-            {
-                item.Next = Items;
-                item.Previous = null;
+            p = head;
+            int nmerges = 0;  //number of merges done this pass
+            int psize, qsize; //lengths of the components we are merging
+            head = null;
 
-                if (Items != null)
+            while (p is not null)
+            {
+                nmerges++;
+                q = p;
+                psize = 0;
+
+                for (int i = 0; i < unitsize; i++)
                 {
-                    Items.Previous = item;
+                    psize++;
+                    q = (T)q.Next;
+
+                    if (q is null)
+                        break;
                 }
 
-                Items = item;
-            }
-            else
-            {
-                LinkedObject next = first.Next;
-                item.Next = next;
-                item.Previous = first;
-                first.Next = item;
+                qsize = unitsize;
 
-                if (next != null)
+                while (psize > 0 || qsize > 0 && q is not null)
                 {
-                    next.Previous = item;
-                }
-            }
-        }
-
-        public void MoveToFront(LinkedObject item)
-        {
-            if (item != null && item != Items)
-            {
-                Unlink(item);
-
-                if (Items != null)
-                {
-                    Items.Previous = item;
-                }
-
-                item.Next = Items;
-                item.Previous = null;
-                Items = item;
-            }
-        }
-
-        public void MoveToBack(LinkedObject item)
-        {
-            if (item != null)
-            {
-                Unlink(item);
-                LinkedObject last = GetLast();
-
-                if (last == null)
-                {
-                    Items = item;
-                }
-                else
-                {
-                    last.Next = item;
-                }
-
-                item.Previous = last;
-                item.Next = null;
-            }
-        }
-
-        public LinkedObject GetLast()
-        {
-            LinkedObject last = Items;
-
-            while (last != null && last.Next != null)
-            {
-                last = last.Next;
-            }
-
-            return last;
-        }
-
-        public void Clear()
-        {
-            if (Items != null)
-            {
-                LinkedObject item = Items;
-                Items = null;
-
-                while (item != null)
-                {
-                    LinkedObject next = item.Next;
-                    item.Next = null;
-                    item = next;
-                }
-            }
-        }
-
-        /// <summary>
-        ///     Sort the contents of this LinkedObject using merge sort.
-        ///     Adapted from Simon Tatham's C implementation: https://www.chiark.greenend.org.uk/~sgtatham/algorithms/listsort.html
-        /// </summary>
-        /// <typeparam name="T">Type of the objects being compared.</typeparam>
-        /// <param name="comparison">Comparison function to use when sorting.</param>
-        public LinkedObject SortContents<T>(Comparison<T> comparison) where T : LinkedObject
-        {
-            if (Items == null)
-            {
-                return null;
-            }
-
-            int unitsize = 1; //size of the components we are merging; 1 for first iteration, multiplied by 2 after each iteration
-
-            T p = null, q = null, e = null, head = (T) Items, tail = null;
-
-            while (true)
-            {
-                p = head;
-                int nmerges = 0;  //number of merges done this pass
-                int psize, qsize; //lengths of the components we are merging
-                head = null;
-                tail = null;
-
-                while (p != null)
-                {
-                    nmerges++;
-                    q = p;
-                    psize = 0;
-
-                    for (int i = 0; i < unitsize; i++)
+                    if (psize == 0)
                     {
-                        psize++;
-                        q = (T) q.Next;
-
-                        if (q == null)
-                        {
-                            break;
-                        }
+                        e = q;
+                        q = (T)q.Next;
+                        qsize--;
+                    }
+                    else if (qsize == 0 || q is null)
+                    {
+                        e = p;
+                        p = (T)p.Next;
+                        psize--;
+                    }
+                    else if (comparison(p, q) <= 0)
+                    {
+                        e = p;
+                        p = (T)p.Next;
+                        psize--;
+                    }
+                    else
+                    {
+                        e = q;
+                        q = (T)q.Next;
+                        qsize--;
                     }
 
-                    qsize = unitsize;
+                    if ((T?)null is not null)
+                        ((T?)null).Next = e;
+                    else
+                        head = e;
 
-                    while (psize > 0 || qsize > 0 && q != null)
-                    {
-                        if (psize == 0)
-                        {
-                            e = q;
-                            q = (T) q.Next;
-                            qsize--;
-                        }
-                        else if (qsize == 0 || q == null)
-                        {
-                            e = p;
-                            p = (T) p.Next;
-                            psize--;
-                        }
-                        else if (comparison(p, q) <= 0)
-                        {
-                            e = p;
-                            p = (T) p.Next;
-                            psize--;
-                        }
-                        else
-                        {
-                            e = q;
-                            q = (T) q.Next;
-                            qsize--;
-                        }
-
-                        if (tail != null)
-                        {
-                            tail.Next = e;
-                        }
-                        else
-                        {
-                            head = e;
-                        }
-
-                        e.Previous = tail;
-                        tail = e;
-                    }
-
-                    p = q;
+                    e.Previous = null;
+                    tail = e;
                 }
 
-                tail.Next = null;
-
-                if (nmerges <= 1)
-                {
-                    Items = head;
-
-                    return head;
-                }
-
-                unitsize *= 2;
+                p = q;
             }
+
+            ((T?)null).Next = null;
+
+            if (nmerges <= 1)
+            {
+                Items = head;
+                return head;
+            }
+
+            unitsize *= 2;
         }
     }
 }
